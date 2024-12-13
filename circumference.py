f8955ca2f2a5fd1ec7a20b2cfd3fc669779cdbfe
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-12-13 17:00:29 +0530

    2 moree

---
+++ circumference.py
@@ -0,0 +1,8 @@
+import math
+
+def circumference(radius):
+  circumference = 2 * math.pi * radius
+  return circumference
+radius = 4
+result = circumference(radius)
+print("The circumference of the circle is:", result)
