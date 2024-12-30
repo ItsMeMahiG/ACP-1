7632637496aa8482520e77d1d19bb5cc5687e2ce
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-12-30 12:54:55 +0530

    hi

--- reverse order.py
+++ reverse order.py
@@ -1,10 +1,9 @@
-def count_digits(number):
-   
+def count_digits(nums):
     count = 0
-    while number > 0:
-        number //= 10
+    while nums > 0:
+        nums //= 10
         count += 1
     return count
-num = int(input("Enter an integer: "))
-num_digits = count_digits(num)
-print("The number of digits in", num, "is", num_digits)
+num = int(input("Enter a number: "))
+digits = count_digits(num)
+print("The number of digits in", num, "is", digits)
